152ba2bca1d9712792e8192c85f3b87461c49046
backslash127 <jae.miller127@icloud.com>
2026-03-23 08:20:21 -0500

    test

--- js/404.js
+++ js/404.js
@@ -4,6 +4,10 @@ if (isIFrame == true) {
     window.top.location.replace("https://" + location.hostname + "/404");
 };
 
+if (window.location != "https://" + location.hostname + "/404") {
+    window.location.replace("https://" + location.hostname + "/404");
+};
+
 document.addEventListener("keypress", function(event) {
     window.location.replace("https://" + location.hostname + "/?p=/home");
 });
